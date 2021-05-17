71f5569806c76ed5546374f06b144f8cd4702528
wangzhe <415677099@qq.com>
2021-05-17 19:56:40 +0800

    ADD： 天气库依赖和调用 天气控件UI修改

--- app/src/main/java/com/alight/android/aoa_launcher/LauncherApplication.kt
+++ app/src/main/java/com/alight/android/aoa_launcher/LauncherApplication.kt
@@ -3,6 +3,7 @@ package com.alight.android.aoa_launcher
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDexApplication
+import com.qweather.sdk.view.HeConfig
 
 class LauncherApplication : MultiDexApplication() {
 
