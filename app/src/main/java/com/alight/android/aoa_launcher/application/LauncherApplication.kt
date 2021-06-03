63a93d0538e4888e2ff12c6dd7bbcf44fdf4b855
wangzhe <415677099@qq.com>
2021-06-03 18:59:06 +0800

    ADD : 增加签名和LauncherActivity作为启动

--- app/src/main/java/com/alight/android/aoa_launcher/application/LauncherApplication.kt
+++ app/src/main/java/com/alight/android/aoa_launcher/application/LauncherApplication.kt
@@ -1,9 +1,8 @@
-package com.alight.android.aoa_launcher
+package com.alight.android.aoa_launcher.application
 
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDexApplication
-import com.qweather.sdk.view.HeConfig
 
 class LauncherApplication : MultiDexApplication() {
 
