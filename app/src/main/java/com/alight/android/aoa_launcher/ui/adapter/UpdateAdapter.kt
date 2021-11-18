7890b424ae7a6119c77a163f30c75fb21d8b8d07
wangzhe <415677099@qq.com>
2021-11-18 17:18:40 +0800

    ADD：进度条UI

--- app/src/main/java/com/alight/android/aoa_launcher/ui/adapter/UpdateAdapter.kt
+++ app/src/main/java/com/alight/android/aoa_launcher/ui/adapter/UpdateAdapter.kt
@@ -1,5 +1,8 @@
 package com.alight.android.aoa_launcher.ui.adapter
 
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.alight.android.aoa_launcher.R
 import com.alight.android.aoa_launcher.net.model.File
 import com.chad.library.adapter.base.BaseQuickAdapter
@@ -10,20 +13,12 @@ class UpdateAdapter : BaseQuickAdapter<File, BaseViewHolder>(R.layout.item_updat
     override fun convert(holder: BaseViewHolder, file: File) {
         holder.setText(R.id.tv_app_name_update_item, file.fileName.split(".")[0])
         holder.setText(R.id.tv_app_size_update_item, file.sizeStr)
-        //孩子头像
-//        val childIcon = holder.getView<ImageView>(R.id.iv_child_icon_splash)
-//        //孩子名称
-//        val childName = holder.getView<TextView>(R.id.tv_child_name_splash)
-////        "gender": 0, // 0 -> 未知， 1 -> male 2-> female
-//        //未知和女性都使用女性头像，否则使用男头像
-//
-//        Glide.with(context)
-//            .load(tokenPair.avatar)
-//            .apply(RequestOptions.bitmapTransform(CircleCrop()))
-//            .error(if (tokenPair.gender == 1) R.drawable.splash_boy else R.drawable.splash_girl)
-//            .into(childIcon)
-
-//        childName.text = tokenPair.name
+        var tvUpdate = holder.getView<TextView>(R.id.tv_update_item)
+        var pbUpdate = holder.getView<ProgressBar>(R.id.pb_update_item)
+        tvUpdate.setOnClickListener {
+            tvUpdate.visibility = View.GONE
+            pbUpdate.visibility = View.VISIBLE
+        }
     }
 
 }
