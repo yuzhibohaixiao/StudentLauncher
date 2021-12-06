0d19cea7e7ce2c379684b1680fd5ba26b7b4de92
wangzhe <415677099@qq.com>
2021-12-06 14:54:56 +0800

    ADD：左侧点击效果

--- app/src/main/java/com/alight/android/aoa_launcher/activity/NewLauncherActivity.kt
+++ app/src/main/java/com/alight/android/aoa_launcher/activity/NewLauncherActivity.kt
@@ -4,6 +4,7 @@ import android.view.View
 import com.alight.android.aoa_launcher.R
 import com.alight.android.aoa_launcher.common.base.BaseActivity
 import com.alight.android.aoa_launcher.presenter.PresenterImpl
+import kotlinx.android.synthetic.main.activity_launcher.*
 
 
 /**
@@ -20,6 +21,11 @@ class NewLauncherActivity : BaseActivity(), View.OnClickListener {
     }
 
     override fun initView() {
+        tv_ar_launcher.setOnClickListener(this)
+        tv_chinese_launcher.setOnClickListener(this)
+        tv_mathematics_launcher.setOnClickListener(this)
+        tv_english_launcher.setOnClickListener(this)
+        tv_quality_launcher.setOnClickListener(this)
     }
 
     override fun initPresenter(): PresenterImpl {
@@ -42,6 +48,20 @@ class NewLauncherActivity : BaseActivity(), View.OnClickListener {
      * @param v The view that was clicked.
      */
     override fun onClick(v: View) {
+        when (v.id) {
+            R.id.tv_ar_launcher, R.id.tv_chinese_launcher, R.id.tv_mathematics_launcher, R.id.tv_english_launcher, R.id.tv_quality_launcher
+            -> {
+                showLeftSelectUi(v.id)
+            }
+        }
+    }
+
+    private fun showLeftSelectUi(id: Int) {
+        iv_ar_launcher.visibility = if (id == R.id.tv_ar_launcher) View.VISIBLE else View.GONE
+        iv_chinese_launcher.visibility = if (id == R.id.tv_chinese_launcher) View.VISIBLE else View.GONE
+        iv_mathematics_launcher.visibility = if (id == R.id.tv_mathematics_launcher) View.VISIBLE else View.GONE
+        iv_english_launcher.visibility = if (id == R.id.tv_english_launcher) View.VISIBLE else View.GONE
+        iv_quality_launcher.visibility = if (id == R.id.tv_quality_launcher) View.VISIBLE else View.GONE
     }
 
 
