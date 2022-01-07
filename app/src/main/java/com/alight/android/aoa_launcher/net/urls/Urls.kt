79da151feecaf37e87828eb229671f184faf743a
wangzhe <415677099@qq.com>
2022-01-07 15:04:00 +0800

    ADD：高清切图修改

--- app/src/main/java/com/alight/android/aoa_launcher/net/urls/Urls.kt
+++ app/src/main/java/com/alight/android/aoa_launcher/net/urls/Urls.kt
@@ -2,7 +2,7 @@ package com.alight.android.aoa_launcher.net.urls
 
 object Urls {
 
-    //            const val BASEURL = "http://api.alight-sys.com"   //正式服务（生产）
+//                const val BASEURL = "http://api.alight-sys.com"   //正式服务（生产）
     const val BASEURL =
         "http://test.api.alight-sys.com" //测试服务
 
