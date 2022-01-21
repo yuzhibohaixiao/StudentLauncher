3eb637c74f9e0f6e82b9cddcd7e87c235ff91dd8
wangzhe <415677099@qq.com>
2022-01-21 14:04:22 +0800

    FIX：音视频偶尔不会被调起的bug

--- app/src/main/java/com/alight/android/aoa_launcher/common/broadcast/JPushDefaultReceiver.java
+++ app/src/main/java/com/alight/android/aoa_launcher/common/broadcast/JPushDefaultReceiver.java
@@ -1,6 +1,7 @@
 package com.alight.android.aoa_launcher.common.broadcast;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -44,10 +45,15 @@ public class JPushDefaultReceiver extends BroadcastReceiver {
         } else if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
             Log.d(TAG, "收到了自定义消息。消息内容是：" + bundle.getString(JPushInterface.EXTRA_MESSAGE));         // 自定义消息不会展示在通知栏，完全要开发者写代码去处理     } else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent.getAction())) {         Log.d(TAG, "收到了通知");         // 在这里可以做些统计，或者做些其他工作     } else if (JPushInterface.ACTION_NOTIFICATION_OPENED.equals(intent.getAction())) {         Log.d(TAG, "用户点击打开了通知");         // 在这里可以自己写代码去定义用户点击后的行为         Intent i = new Intent(context, TestActivity.class);  //自定义打开的界面         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);         context.startActivity(i);     } else {         Log.d(TAG, "Unhandled intent - " + intent.getAction());    } }
         } else if (JPushInterface.ACTION_NOTIFICATION_RECEIVED.equals(intent.getAction())) {
+            Log.d(TAG, "收到了音视频。消息内容是：" + bundle.getString(JPushInterface.EXTRA_MESSAGE));
             String json = bundle.getString(JPushInterface.EXTRA_EXTRA);
             try {
                 CallArBean callArBean = new Gson().fromJson(json, CallArBean.class);
-                Intent intent2 = new Intent("com.alight.trtcav.WindowActivity");
+                Intent intent2 = new Intent();
+                intent2.setComponent(new ComponentName(
+                        "com.tencent.trtcav",
+                        "com.alight.trtcav.activity.WindowActivity"
+                ));
                 intent2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent2.putExtra("parentId", callArBean.getMessage().getFromUserId() + "");
                 intent2.putExtra("parentName", callArBean.getMessage().getFromUserInfo().getName());
@@ -64,6 +70,7 @@ public class JPushDefaultReceiver extends BroadcastReceiver {
                 }
                 sendUserUseAv(context);
                 handler.postDelayed(() -> {
+                    Log.d(TAG, "唤起音视频");
                     context.startActivity(intent2);
                 }, 100);
 
