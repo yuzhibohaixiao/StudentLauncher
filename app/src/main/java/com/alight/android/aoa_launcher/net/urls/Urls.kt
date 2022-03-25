2e8dc6a178eddcc402ec3c863c606ae3452ac3bb
wangzhe <415677099@qq.com>
2022-03-25 16:56:42 +0800

    FIX: 删除不必要的权限

--- app/src/main/java/com/alight/android/aoa_launcher/net/urls/Urls.kt
+++ app/src/main/java/com/alight/android/aoa_launcher/net/urls/Urls.kt
@@ -4,8 +4,8 @@ object Urls {
 
     //          android 9 以上需要使用https
 //  const val BASEURL = "https://api.alight-sys.com"   //正式服务（生产）
-//    const val BASEURL = "https://test.api.alight-sys.com" //测试服务
-    const val BASEURL = "https://appotronics.api.alight-sys.com" //光峰
+    const val BASEURL = "https://test.api.alight-sys.com" //测试服务
+//    const val BASEURL = "https://appotronics.api.alight-sys.com" //光峰
 
 //  const val BASEURL = "http://api.alight-sys.com"   //正式服务（生产）
 //  const val BASEURL = "http://test.api.alight-sys.com" //测试服务
