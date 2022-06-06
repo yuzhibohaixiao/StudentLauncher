643da71964b3bba7a320160337bbd06c785a9252
415677099@qq.com <wangzhe333>
2022-06-06 18:02:41 +0800

    ADD: wifi模块初始化，列表获取

--- app/src/main/java/com/alight/android/aoa_launcher/activity/WifiActivity.kt
+++ app/src/main/java/com/alight/android/aoa_launcher/activity/WifiActivity.kt
@@ -1,18 +1,68 @@
 package com.alight.android.aoa_launcher.activity
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.wifi.WifiManager
 import com.alight.android.aoa_launcher.R
 import com.alight.android.aoa_launcher.common.base.BaseActivity
 import com.alight.android.aoa_launcher.presenter.PresenterImpl
+import kotlinx.android.synthetic.main.activity_wifi.*
 
 class WifiActivity : BaseActivity() {
-    override fun setListener() {
+
+    val wifiManager = application.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+    val wifiScanReceiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            val success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
+            if (success) {
+                scanSuccess()
+            } else {
+                scanFailure()
+            }
+        }
+    }
+
+    private fun scanSuccess() {
+        val results = wifiManager.scanResults
+//        ... use new scan results ...
+    }
+
+    private fun scanFailure() {
+        // handle failure: new scan did NOT succeed
+        // consider using old scan results: these are the OLD results!
+        val results = wifiManager.scanResults
+//        ... potentially use older scan results ...
     }
 
     override fun initData() {
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+        registerReceiver(wifiScanReceiver, intentFilter)
+
+        val success = wifiManager.startScan()
+        if (!success) {
+            // scan failure handling
+            scanFailure()
+        }
+
+        val scanResults = wifiManager.scanResults
     }
 
     override fun initView() {
     }
+
+    override fun setListener() {
+        switch_wifi.setOnCheckedChangeListener { buttonView, isChecked ->
+//            if (isChecked) {
+//            } else {
+//            }
+        }
+    }
+
 
     override fun initPresenter(): PresenterImpl {
         return PresenterImpl()
@@ -27,4 +77,14 @@ class WifiActivity : BaseActivity() {
 
     override fun onError(error: String) {
     }
+
+/*
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.switch_wifi -> {
+
+            }
+        }
+    }
+*/
 }
