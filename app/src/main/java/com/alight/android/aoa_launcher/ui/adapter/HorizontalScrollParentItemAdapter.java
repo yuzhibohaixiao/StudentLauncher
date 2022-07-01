d79d5d533307901adb6d8764531a2a12ffdca9fa
415677099@qq.com <wangzhe333>
2022-07-01 11:17:30 +0800

    ADD: 家长信息展示的UI完善

--- app/src/main/java/com/alight/android/aoa_launcher/ui/adapter/HorizontalScrollParentItemAdapter.java
+++ app/src/main/java/com/alight/android/aoa_launcher/ui/adapter/HorizontalScrollParentItemAdapter.java
@@ -1,7 +1,6 @@
 package com.alight.android.aoa_launcher.ui.adapter;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +11,11 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.alight.android.aoa_launcher.R;
-import com.alight.android.aoa_launcher.common.bean.AppBean;
 import com.alight.android.aoa_launcher.common.bean.Parent;
 import com.alight.android.aoa_launcher.utils.ToastUtils;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.RequestOptions;
 
 import java.util.List;
 
@@ -30,14 +31,14 @@ public class HorizontalScrollParentItemAdapter extends RecyclerView.Adapter<Hori
     @NonNull
     @Override
     public HorizontalItemHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_dialog_launcher, viewGroup, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.item_dialog_home_parent, viewGroup, false);
         return new HorizontalItemHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull HorizontalItemHolder viewHolder, int position) {
         Parent itemBean = itemBeans.get(position);
-        viewHolder.setItem(itemBean);
+        viewHolder.setItem(context, itemBean);
         viewHolder.itemView.setOnClickListener(v -> {
             ToastUtils.showShort(context, "点我干嘛");
 //            Intent intent = context.getPackageManager().getLaunchIntentForPackage(itemBean.getAppPackName());
@@ -60,9 +61,12 @@ public class HorizontalScrollParentItemAdapter extends RecyclerView.Adapter<Hori
             verticalText = itemView.findViewById(R.id.verticalText);
         }
 
-        public void setItem(Parent Parent) {
-//            verticalImgView.setImageDrawable(Parent.getAppIcon());
-//            verticalText.setText(Parent.getAppName());
+        public void setItem(Context context, Parent parent) {
+            Glide.with(context).load(parent.getAvatar())
+                    .error((parent.getRole_type() == 1) ? R.drawable.father : R.drawable.mather)
+                    .apply(RequestOptions.bitmapTransform(new CircleCrop()))
+                    .into(verticalImgView);
+            verticalText.setText(parent.getName());
         }
     }
 }
